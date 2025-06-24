9048fa483a914fecaf07a0369760f7c5d595ff66
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-06-24 16:07:01 +0300

    worked on robotContainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,6 +10,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
@@ -29,4 +31,20 @@ public class Constants {
       public static final int TELEOP_TIME = 135;
       public static final int ENDGAME_TIME = 30;
    }
+
+     public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
+
 }
