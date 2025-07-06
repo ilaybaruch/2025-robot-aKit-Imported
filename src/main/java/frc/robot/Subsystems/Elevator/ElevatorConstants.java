28b34fd0243f4a7ef260da07ee0dbbfee379e313
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-07-06 20:00:37 +0300

    need to tune values

--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
@@ -3,7 +3,7 @@ package frc.robot.Subsystems.Elevator;
 public class ElevatorConstants {
 
     public static final int MOTOR_ID = 16;
-    public static final int LIMIT_SWITCH_ID = 0;
+    public static final int LIMIT_SWITCH_ID = 11;
 
     public static final double kP = 0;
     public static final double kI = 0;
@@ -14,9 +14,12 @@ public class ElevatorConstants {
     public static final double kG = 0;
     public static final double kV = 0;
 
+    public static final double L2POS = 0;
+    public static final double L3POS = 0;
+
     public static final boolean INVERTED = true;
-    public static final int CURRENT_LIMIT = 0;
-    public static final double VOLTAGE_COMPENSATION = 0;
+    public static final int CURRENT_LIMIT = 20;
+    public static final double VOLTAGE_COMPENSATION = 12;
     public static final double POSITION_CONVERSION_FACTOR = 0;
     public static final double MAX_ACCELERATION = 0;
     public static final double MAX_VELOCITY = 0;
