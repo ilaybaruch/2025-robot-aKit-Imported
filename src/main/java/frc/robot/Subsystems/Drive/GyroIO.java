77bae1591153b3f0945e82c817acd09db32035fe
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-08-09 03:37:44 +0300

    worked on pigeon

---
+++ src/main/java/frc/robot/Subsystems/Drive/GyroIO.java
@@ -0,0 +1,34 @@
+package frc.robot.Subsystems.Drive;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.AngularVelocity;
+
+public interface GyroIO {
+    @AutoLog
+
+    public static class GyroIOInputs {
+        public boolean connected = false;
+        public Rotation2d yawPosition = new Rotation2d();
+
+    }
+
+    public default Rotation2d getGyroRotation() {
+        return new Rotation2d();
+    }
+
+    public default AngularVelocity getGyroAngularVelocity() {
+        return null;
+    }
+
+    public default void reset() {
+    }
+
+    public default void reset(Rotation2d to) {
+    }
+
+    public default void updateInputs(GyroIOInputs inputs) {
+    }
+
+}
---
+++ src/main/java/frc/robot/Subsystems/Drive/GyroIOPigeon.java
@@ -0,0 +1,40 @@
+package frc.robot.Subsystems.Drive;
+
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
+
+public class GyroIOPigeon implements GyroIO {
+
+    private final WPI_PigeonIMU pigeon;
+    private Rotation2d offset;
+
+    public GyroIOPigeon() {
+        pigeon = new WPI_PigeonIMU(0);
+        offset = new Rotation2d();
+    }
+
+    @Override
+    public Rotation2d getGyroRotation() {
+        return pigeon.getRotation2d();
+    }
+
+    @Override
+    public AngularVelocity getGyroAngularVelocity() {
+        return AngularVelocity.ofBaseUnits(Units.degreesToRadians(pigeon.getRate()), RadiansPerSecond);
+    }
+
+    @Override
+    public void reset() {
+        reset(new Rotation2d());
+    }
+
+    @Override
+    public void reset(Rotation2d to) {
+        offset = pigeon.getRotation2d().minus(to);
+    }
+}
